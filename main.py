5df1e9423487e03365c4b82f13899565b80968d3
EliotXu <zhenjinxu@shu.edu.cn>
2021-04-13 11:18:45 +0800

    final workflows

--- main.py
+++ main.py
@@ -1,79 +1,60 @@
 import requests
 import re
-import datetime
-
-now_time = datetime.datetime.now()
-bj_time = now_time + datetime.timedelta(hours=8)
 
 msgList_School = ['No Updates']
 msgList_Department = ['No Updates']
 schSign = 0
 departSign = 0
-key = input()  # server酱推送
 
-# ----------------------------------------------------------------
-timeStr_School = bj_time.strftime("%m-%d")
+timeStr_School = "03-27"
+timeStr_Department = "2021-03-31"
+key = input()
 
 headers0 = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36 Edg/89.0.774.54',
     'referer': r'https://aerospace.xmu.edu.cn/index.htm',
     'Upgrade-Insecure-Requests': '1'}
 
-url = 'https://aerospace.xmu.edu.cn/jwxx/yjsjw.htm'
-data = requests.get(url, headers=headers0).content.decode('utf-8')
+url0 = 'https://aerospace.xmu.edu.cn/jwxx/yjsjw.htm'
+data0 = requests.get(url0, headers=headers0).content.decode('utf-8')
 
 pattern0 = re.compile(r'<span>(.*)</span>')
-result = (re.findall(pattern0, data))
+result = (re.findall(pattern0, data0))[1]
 
-if timeStr_School in result:
+if result == timeStr_School:
     schSign = 1
-    pattern_title0 = re.compile(r'<span>' + timeStr_School + '</span>.*>(.+)</a>')
-    msgList_School = (re.findall(pattern_title0, data))
+    pattern_title0 = re.compile(r'<span>' + result + '</span>.*>(.+)</a>')
+    msgList_School = (re.findall(pattern_title0, data0))
 
-# ----------------------------------------------------------------
-
-timeStr_Department = bj_time.strftime("%Y-%m-%d")
-
-headers1 = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36 Edg/89.0.774.54',
-    'referer': r'https://jdx.xmu.edu.cn/',
-    'Upgrade-Insecure-Requests': '1'}
-
-url = 'https://jdx.xmu.edu.cn/xwgg/tzgg.htm'
-data = (requests.get(url, headers=headers1).content.decode('utf-8'))
+# -----------------------------------------------------------------
+url1 = 'https://jdx.xmu.edu.cn/xwgg/tzgg.htm'
+data1 = (requests.get(url1, headers=headers0).content.decode('utf-8'))
 
 pattern1 = re.compile(r'\w{4}-\w{2}-\w{2}')
-result = (re.findall(pattern1, data))
+result1 = (re.findall(pattern1, data1))
+newsNum = result1.count(result1[0])
 
-cnt = result.count(timeStr_Department)
-if cnt:
+if result1[0] == timeStr_Department:
     departSign = 1
     pattern_title1 = re.compile('<a.*?href=.*?>(.{5,})(?=</a>\r\n)')
-    # 此处不认 $, 只认 \r\n <linux下换行>
-    result_title = (re.findall(pattern_title1, data))
-
-    msgList_Department = result_title[:cnt]
+    msgList_Department = (re.findall(pattern_title1, data1))[:newsNum]
 
 # --------------------------------------------
 
 desp = f"""
 ------
-### 🚁Now：
-```
-{bj_time.strftime("%Y-%m-%d %H:%M:%S %p")}
-```
 ### ✨学院新闻：
 ```
- New_Msg:{msgList_School}
+{msgList_School}
 ```
 ### 🚀系新闻:
 ```
- New_Msg:{msgList_Department}
+{msgList_Department}
 ```
 """
 
 if schSign or departSign:
     requests.post('https://sc.ftqq.com/' + key + '.send', data={
-        'text': bj_time.strftime("%Y-%m-%d %H:%M:%S %p") + ' 航院 Updates',
+        'text': 'News Updates',
         'desp': desp
     })
